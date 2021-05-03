04252e4090c880637d2187581a32287a9187ef61
gookgookJ <sksmsanj32@gmail.com>
2021-05-03 20:58:09 +0900

    fix server_index.js_

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ router.post("/signup", indexControllers.signup);
 router.post("/signout", indexControllers.signout);
 router.post("/menu-choice", indexControllers.menu_choice_post);
 router.patch("/menu-choice", indexControllers.menu_choice_patch);
-router.get("/mypage", indexControllers.mypage);
+router.get("/mypage", indexControllers.mypage_get);
+router.patch("/mypage", indexControllers.mypage_patch);
 
 module.exports = router;
