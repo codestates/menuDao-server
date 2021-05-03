580972f6643f244557e982d17af02007ce1730e5
gookgookJ <sksmsanj32@gmail.com>
2021-05-03 20:57:20 +0900

    creted server_index.js_patchMypage

--- controllers/indexCR/index.js
+++ controllers/indexCR/index.js
@@ -49,27 +49,6 @@ module.exports = {
         res.status(401).send({ message: "Invalid user or Wrong password" });
         console.error(err);
       });
-
-    // if (!userInfo) {
-    //   res.status(401).send({ message: "Invalid user or Wrong password" });
-    // } else {
-    //   const { ACCESS_SECRET } = process.env;
-    //   const { REFRESH_SECRET } = process.env;
-    //   const accessToken = jwt.sign(userInfo.dataValues, ACCESS_SECRET, {
-    //     expiresIn: "1 days",
-    //   });
-    //   const refreshToken = jwt.sign(userInfo.dataValues, REFRESH_SECRET, {
-    //     expiresIn: "7 days",
-    //   });
-    //   userInfo.refreshToken = refreshToken;
-    //   res
-    //     .cookie("refreshToken", userInfo.refreshToken, {
-    //       maxAge: 1000 * 60 * 60 * 24 * 7, // 7일간 유지
-    //       httpOnly: true,
-    //     })
-    //     .status(200)
-    //     .send({ accessToken: accessToken });
-    // }
   },
 
   signup: async (req, res) => {
@@ -103,11 +82,7 @@ module.exports = {
 
   signout: (req, res) => {},
 
-  menu_choice_post: (req, res) => {},
-
-  menu_choice_patch: (req, res) => {},
-
-  mypage: async (req, res) => {
+  mypage_get: async (req, res) => {
     const { ACCESS_SECRET } = process.env;
     const { REFRESH_SECRET } = process.env;
     const authorization = req.headers["authorization"];
@@ -135,33 +110,38 @@ module.exports = {
           console.error(err);
         });
     }
-
-    // 엑세스 토큰을 헤더에 담아서 넘겨줌.
-    // const accessToken = req.headers["accessToken"];
-    // if (!accessToken) {
-    //   res.status(400).send({ message: "You do not have access rights" });
-    // } else {
-    //   const { ACCESS_SECRET } = process.env;
-    //   // const { REFRESH_SECRET } = process.env;
-    //   // barer 랑 토큰 분리
-    //   const token = accessToken.split(" ")[1];
-    //   // 분리된 개별 토큰을 복호화
-    //   const data = jwt.verify(token, ACCESS_SECRET, (err, decoded) => {
-    //     // 복호화에 실패했을 경우
-    //     if (err) {
-    // return res
-    //   .status(400)
-    //   .send({ message: "You do not have access rights" });
-    //     } else {
-    //       res.status(201).send({
-    //         message: "successfully get user infomation",
-    //         user_name: data.user_name,
-    //         user_birthday: data.user_birthday,
-    //         user_sex: data.user_sex,
-    //         user_id: data.user_id,
-    //       });
-    //     }
-    //   });
-    // }
   },
+  // 비밀번호 변경 구현 완료
+  mypage_patch: async (req, res) => {
+    const { ACCESS_SECRET } = process.env;
+    const { REFRESH_SECRET } = process.env;
+    const authorization = req.headers["authorization"];
+    const token = authorization.split(" ")[1];
+    const decoded = jwt.verify(token, ACCESS_SECRET);
+    if (!decoded) {
+      return res.status(400).send({ message: "You do not have access rights" });
+    } else {
+      await Users.findOne({ where: { user_id: decoded.userdata.user_id } })
+        .then((result) =>
+          result.update({ user_password: req.body.user_password })
+        )
+        .then(() => {
+          res.status(201).send({
+            message: "successfully update user infomation",
+          });
+        })
+        .catch((err) => {
+          res.status(400).send({
+            message: "You do not have access rights",
+            decoded: decoded,
+          });
+          console.error(err);
+        });
+    }
+  },
+
+  menu_choice_patch: (req, res) => {},
+
+  menu_choice_post: (req, res) => {},
+  // 대분류, 날씨, 기분에 따른 112개의 조건문 분기
 };
