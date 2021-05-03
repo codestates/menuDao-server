846b2594bc653dfb7cacc50b8162922745d03886
gookgookJ <sksmsanj32@gmail.com>
2021-05-04 00:50:06 +0900

    fix server_controllers

--- controllers/diaryCR/index.js
+++ controllers/diaryCR/index.js
@@ -1,9 +1,98 @@
 const { Diary } = require("../../models");
+const { Users } = require("../../models");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
-  diary_get: (req, res) => {},
+  diary_get: async (req, res) => {
+    const { ACCESS_SECRET } = process.env;
+    const { REFRESH_SECRET } = process.env;
+    const authorization = req.headers["authorization"];
+    const token = authorization.split(" ")[1];
+    const decoded = jwt.verify(token, ACCESS_SECRET);
+    if (!decoded) {
+      return res.status(400).send({ message: "You do not have access rights" });
+    } else {
+      // diary_id = PK
+      await Diary.findOne({ where: { id: req.body.diary_id } })
+        .then((result) =>
+          res.status(201).send({
+            message: "succesfully get user diary",
+            diarydata: result.userdata,
+          })
+        )
+        .then(() => {
+          res.status(201).send({
+            message: "successfully diary posting",
+          });
+        })
+        .catch((err) => {
+          res.status(400).send({
+            message: "You do not have access rights",
+            decoded: decoded,
+          });
+          console.error(err);
+        });
+    }
+  },
 
-  diary_post: (req, res) => {},
+  diary_post: async (req, res) => {
+    const { ACCESS_SECRET } = process.env;
+    const { REFRESH_SECRET } = process.env;
+    const authorization = req.headers["authorization"];
+    const token = authorization.split(" ")[1];
+    const decoded = jwt.verify(token, ACCESS_SECRET);
+    if (!decoded) {
+      await res.status(400).send({ message: "You do not have access rights" });
+    } else {
+      await Users.findOne({ where: { user_id: decoded.userdata.user_id } })
+        .then((result) =>
+          Diary.create({
+            users_id: result.dataValues.id,
+            comment: "내용을 입력해주세요",
+            data: req.body.data,
+            weather: req.body.weather,
+            feeling_name: req.body.feeling,
+            big_choice_menu: req.body.big_choice_menu,
+          })
+        )
+        .then(() => {
+          res.status(201).send({
+            message: "successfully diary posting",
+          });
+        })
+        .catch((err) => {
+          res.status(400).send({
+            message: "You do not have access rights",
+            decoded: decoded,
+          });
+          console.error(err);
+        });
+    }
+  },
 
-  diary_patch: (req, res) => {},
+  diary_patch: async (req, res) => {
+    const { ACCESS_SECRET } = process.env;
+    const { REFRESH_SECRET } = process.env;
+    const authorization = req.headers["authorization"];
+    const token = authorization.split(" ")[1];
+    const decoded = jwt.verify(token, ACCESS_SECRET);
+    if (!decoded) {
+      return res.status(400).send({ message: "You do not have access rights" });
+    } else {
+      await Diary.findOne({ where: { id: req.body.diary_id } })
+        .then((result) => result.update({ comment: req.body.comment }))
+        .then(() => {
+          res.status(201).send({
+            message: "succesfully update diary",
+          });
+        })
+        .catch((err) => {
+          res.status(400).send({
+            message: "You do not have access rights",
+            decoded: decoded,
+          });
+          console.error(err);
+        });
+    }
+  },
 };
--- controllers/diaryListCR/index.js
+++ controllers/diaryListCR/index.js
@@ -1,7 +1,39 @@
 const { Diary } = require("../../models");
+const { Users } = require("../../models");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
-  diary_list_get: (req, res) => {},
+  diary_list_get: async (req, res) => {
+    const { ACCESS_SECRET } = process.env;
+    const { REFRESH_SECRET } = process.env;
+    const authorization = req.headers["authorization"];
+    const token = authorization.split(" ")[1];
+    const decoded = jwt.verify(token, ACCESS_SECRET);
+    if (!decoded) {
+      return res.status(400).send({ message: "You do not have access rights" });
+    } else {
+      await Users.findOne({ where: { user_id: decoded.userdata.user_id } })
+        .then((result) =>
+          Diary.findAll({
+            where: { users_id: result.userdata.id },
+          }).then((datas) => {
+            console.log;
+          })
+        )
+        .then(() => {
+          res.status(201).send({
+            message: "successfully update user infomation",
+          });
+        })
+        .catch((err) => {
+          res.status(400).send({
+            message: "You do not have access rights",
+            decoded: decoded,
+          });
+          console.error(err);
+        });
+    }
+  },
 
   diary_list_delete: (req, res) => {},
 };
