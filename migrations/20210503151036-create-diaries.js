65399e01b9ecab6002a9376986908c826272dfb6
gookgookJ <sksmsanj32@gmail.com>
2021-05-04 18:14:44 +0900

    create Server_diaries_migrations

--- migrations/20210503151036-create-diaries.js
+++ migrations/20210503151036-create-diaries.js
@@ -1,39 +1,39 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Diaries', {
+    await queryInterface.createTable("Diaries", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       date: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       feeling: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       weather: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       choice_menu: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       users_id: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Diaries');
-  }
+    await queryInterface.dropTable("Diaries");
+  },
 };
