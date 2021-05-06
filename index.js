dfac1a09e25d99a952420ea862efbec09c7a15a4
HPROGRAM <wjs2096@gmail.com>
2021-05-06 20:05:02 +0900

    fix Server_main index.js

--- index.js
+++ index.js
@@ -3,19 +3,32 @@
 require("dotenv").config();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-
+const mysql = require("mysql");
 const express = require("express");
 const app = express();
 
 const indexRouter = require("./routes/index");
 const diaryRouter = require("./routes/diary");
 const diaryListRouter = require("./routes/diaryList");
+const { NONE } = require("sequelize");
+
+const options = {
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD, //RDS 비번 ,
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
+  dialect: "mysql",
+  port: 3306,
+};
+
+const conn = mysql.createConnection(options);
+conn.connect();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); // 이중객체 처리를 위해.
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
   })
@@ -26,11 +39,11 @@ app.use("/", indexRouter);
 app.use("/diary", diaryRouter);
 app.use("/diary-list", diaryListRouter);
 
-const HTTP_PORT = process.env.HTTP_PORT || 4000;
+const HTTP_PORT = process.env.HTTP_PORT || 80;
 
 let server;
 
 server = app.listen(HTTP_PORT, () => {
-  console.log("서버가 4000번포트에서 작동중");
+  console.log(`서버가 ${HTTP_PORT}번에서 작동중입니다.`);
 });
 module.exports = server;
